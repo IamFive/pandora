0bea4f5843b45155d3caa64ee5eeec4e4a156aa4
Woo Cubic <iampurse@vip.qq.com>
2017-12-12 15:55:48 +0800

    ignore ssl verify

--- src/main/java/net/turnbig/pandora/httpclient/ProxiedHttpClientBuilder.java
+++ src/main/java/net/turnbig/pandora/httpclient/ProxiedHttpClientBuilder.java
@@ -2,6 +2,8 @@ package net.turnbig.pandora.httpclient;
 
 import java.net.InetSocketAddress;
 
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -19,12 +21,23 @@ public class ProxiedHttpClientBuilder {
 
 	public static final String PROXY_SOCKS_ADDRESS_ATTR = "proxy.socks.address";
 
+	public static PoolingHttpClientConnectionManager createConnectionManager(SSLContext sslContext) {
+		sslContext = sslContext != null ? sslContext: SSLContexts.createDefault();
+		Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory> create()
+				.register("http", new ProxiedPlainConnectionSocketFactory())
+				.register("https", new ProxiedSSLConnectionSocketFactory(sslContext)).build();
+		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg);
+		return cm;
+	}
+	
 	public static PoolingHttpClientConnectionManager createConnectionManager() {
 		Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory> create()
 				.register("http", new ProxiedPlainConnectionSocketFactory())
 				.register("https", new ProxiedSSLConnectionSocketFactory(SSLContexts.createDefault())).build();
-		return new PoolingHttpClientConnectionManager(reg);
+		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg);
+		return cm;
 	}
+
 
 	public static HttpContext createHttpContext(String host, Integer port) {
 		InetSocketAddress proxySocketAddr = new InetSocketAddress(host, port);
